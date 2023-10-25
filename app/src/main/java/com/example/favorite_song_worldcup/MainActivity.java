4da41f1f77b4aadef1e19a257e25b80d3e959a97
BOOK-GFT8ELJK47\jaehe <jaeheon0520@naver.com>
2023-10-25 23:34:26 +0900

    "프로젝트 테스트"

--- app/src/main/java/com/example/favorite_song_worldcup/MainActivity.java
+++ app/src/main/java/com/example/favorite_song_worldcup/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.favorite_song_worldcup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
